6c9c35d7da25b7179e41e23bdb198c72f2695503
wailBouter <ew_bouterbiat@esi.dz>
2020-03-28 23:06:35 +0100

    Implementing messages

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
+const socket = io();
+
+// Message from server
+socket.on('message', message => {
+    console.log(message.text);
+    outputMessage(message);
+
+    // Scroll down
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+})
+
+// Message submit
+chatForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // Get Message text
+    const msg = e.target.elements.msg.value;
+
+    // Emit message to server
+    socket.emit('chatMessage', msg);
+
+    // Clear input
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+})
+
+// Output message to Dom
+function outputMessage(message) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `	<p class="meta">${message.username} <span>${message.time}</span></p>
+    <p class="text">
+        ${message.text}
+    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+    
+}
---
+++ server.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+const botName = 'ChatCord Bot';
+
+// Run when client connects
+io.on('connection', socket => {
+    // Welcome current user
+    socket.emit('message', formatMessage(botName, 'Welcome to ChatCord!'));
+
+    //Broadcast when a user connects
+    socket.broadcast.emit('message', formatMessage(botName, 'A user has joined the chat'));
+
+    // Runs when client disconnects
+    socket.on('disconnect', () => {
+        io.emit('message', formatMessage(botName, 'A user has left the chat'));
+    });
+
+    // Listen for chatMessage
+    socket.on('chatMessage', msg => {
+        io.emit('message', formatMessage('User', msg));
+    });
+});
+const PORT = 3000 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
